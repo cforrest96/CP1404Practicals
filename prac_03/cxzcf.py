ea0efd76064d3b60ef8f1d9f3721f2f3d92241a5
caleb.forrest@my.jcu.edu.au <caleb2626>
2018-03-15 15:16:11 +1000

    Prac_03 tweaks and add Prac_04

--- prac_03/cxzcf.py
+++
@@ -1,9 +0,0 @@
-import string
-
-def count_letters(str):
-    """Return the count of letters in str"""
-    count = 0
-    for char in str:
-        if char.lower() in string.ascii_lowercase:
-            count += 1
-    return count
